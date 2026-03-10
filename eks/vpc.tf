653de28917a565cc329bb7b76d26aa7c41dd2248
diptendusre <diptendusre@gmail.com>
2026-03-11 01:28:03 +0530

    updated with aws module of vpc

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -3,10 +3,17 @@ resource "random_id" "name" {
 }
 
 module "vpc_example_complete" {
-  source  = "terraform-aws-modules/vpc/aws//examples/complete"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "6.6.0"
 
-  name="diptendu-eks-vpc-${random_id.name.id}"
+  name = "diptendu-eks-vpc-${random_id.name.id}"
   region = var.region
   cidr = var.cidrblock
+  azs = ["${var.region}a","${var.region}b"]
+  public_subnets = ["10.0.1.0/24","10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24","10.0.4.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+  enable_dns_support = true
 }
