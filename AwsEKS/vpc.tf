293776fa338fcc7ee119049b84dfd7e0432bae56
diptendusre <diptendusre@gmail.com>
2026-03-12 23:09:07 +0530

    EKS Cluster

---
+++ AwsEKS/vpc.tf
@@ -0,0 +1,22 @@
+resource "random_id" "eks" {
+  byte_length = 8
+}
+
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc-eks"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
