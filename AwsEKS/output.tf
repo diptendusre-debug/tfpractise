5d311c77e4199bc0837b8033a925707e81f86edb
diptendusre <diptendusre@gmail.com>
2026-03-12 23:39:03 +0530

    Updated with SG

--- AwsEKS/output.tf
+++ AwsEKS/output.tf
@@ -1,15 +1,12 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
 output "natgw_ids" {
   value = module.vpc.natgw_ids
 }
-output "igw_id"{
-    value = module.vpc.igw_id      
+output "igw_id" {
+  value = module.vpc.igw_id
+}
+output "security_group_id" {
+  value = module.vpc.default_security_group_id
 }
--- AwsEKS/vpc.tf
+++ AwsEKS/vpc.tf
@@ -9,14 +9,37 @@ module "vpc" {
   name = "my-vpc-eks"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs                = ["us-east-1a", "us-east-1b"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
+}
+resource "aws_security_group" "eks_security_group" {
+  name        = "eks_security_group"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = module.vpc.vpc_id
+  tags = {
+    Name = "eks_security_group"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.eks_security_group.id
+  cidr_ipv4         = module.vpc.vpc_cidr_block
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 0
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.eks_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
