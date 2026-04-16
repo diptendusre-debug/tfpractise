817f7c96f0691856f121d65b5e39206b26401c98
diptendusre <diptendusre@gmail.com>
2026-04-16 08:13:11 +0530

    day09

---
+++ day09/variables.tf
@@ -0,0 +1,15 @@
+variable amiid{
+  description = "It is to give ami id "
+  type=string
+  default="ami-0b6c6ebed2801a5cb"
+}
+variable instancetype{
+  description="This is to give instance type"
+  type=string
+  default="t3.micro"
+}
+variable tagname{
+  description="This is to provide tagname"
+  type=string
+  default="myec2"
+}
